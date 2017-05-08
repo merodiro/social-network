75ef79722137c63377cbbd689d7e7e457a8f0f1c
merodiro <merodiro@gmail.com>
2017-05-08 04:03:18 +0000

    Apply fixes from StyleCI

--- tests/Unit/FriendshipTest.php
+++ tests/Unit/FriendshipTest.php
@@ -2,10 +2,8 @@
 
 namespace Tests\Unit;
 
-use App\User;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
 class FriendshipTest extends TestCase
 {
@@ -15,7 +13,7 @@ class FriendshipTest extends TestCase
     public function user_can_send_a_friend_request()
     {
         $sender = factory('App\User')->create();
-        $recipient  = factory('App\User')->create();
+        $recipient = factory('App\User')->create();
 
         $sender->addFriend($recipient);
 
@@ -27,20 +25,20 @@ class FriendshipTest extends TestCase
     public function user_can_not_send_a_friend_request_if_frienship_is_pending()
     {
         $sender = factory('App\User')->create();
-        $recipient  = factory('App\User')->create();
+        $recipient = factory('App\User')->create();
 
         $sender->addFriend($recipient);
         $sender->addFriend($recipient);
         $sender->addFriend($recipient);
-        
-        $this->assertCount(1, $recipient->friendRequestFrom());        
+
+        $this->assertCount(1, $recipient->friendRequestFrom());
     }
 
     /** @test */
     public function user_can_send_a_friend_request_if_frienship_is_denied()
     {
         $sender = factory('App\User')->create();
-        $recipient  = factory('App\User')->create();
+        $recipient = factory('App\User')->create();
 
         $sender->addFriend($recipient);
 
@@ -48,15 +46,15 @@ class FriendshipTest extends TestCase
         $this->assertCount(0, $recipient->friendRequestFrom());
 
         $sender->addFriend($recipient);
-        $this->assertCount(1, $recipient->friendRequestFrom());        
+        $this->assertCount(1, $recipient->friendRequestFrom());
     }
 
     /** @test */
     public function user_can_remove_a_friend_request()
     {
         $sender = factory('App\User')->create();
-        $recipient  = factory('App\User')->create();        
-        
+        $recipient = factory('App\User')->create();
+
         $sender->addFriend($recipient);
         $sender->deleteFriend($recipient);
         $this->assertCount(0, $recipient->friendRequestFrom());
@@ -96,7 +94,7 @@ class FriendshipTest extends TestCase
         $this->assertEquals('friends', $sender->checkFriendship($recipient));
     }
 
-     /** @test */
+    /** @test */
     public function user_has_not_friend_request_from_if_he_accepted_the_friend_request()
     {
         $sender = factory('App\User')->create();
@@ -120,7 +118,7 @@ class FriendshipTest extends TestCase
         $this->assertEquals('pending', $recipient->checkFriendship($sender));
     }
 
-     /** @test */
+    /** @test */
     public function it_returns_accepted_friendships()
     {
         $sender = factory('App\User')->create();
@@ -134,7 +132,7 @@ class FriendshipTest extends TestCase
         $recipients[1]->acceptFriend($sender);
         $recipients[2]->deleteFriend($sender);
 
-        $this->assertCount(2 , $sender->friends());
+        $this->assertCount(2, $sender->friends());
     }
 
     /** @test */
@@ -151,17 +149,17 @@ class FriendshipTest extends TestCase
         $recipients[1]->acceptFriend($sender);
         $recipients[2]->deleteFriend($sender);
 
-        $this->assertCount(2 , $sender->friends());
+        $this->assertCount(2, $sender->friends());
 
-        $this->assertCount(1 , $recipients[0]->friends());
-        $this->assertCount(1 , $recipients[1]->friends());
-        $this->assertCount(0 , $recipients[2]->friends());
-        $this->assertCount(0 , $recipients[3]->friends());
+        $this->assertCount(1, $recipients[0]->friends());
+        $this->assertCount(1, $recipients[1]->friends());
+        $this->assertCount(0, $recipients[2]->friends());
+        $this->assertCount(0, $recipients[3]->friends());
 
         $this->containsOnlyInstancesOf(\App\User::class, $sender->friends());
     }
 
-     /** @test */
+    /** @test */
     public function it_returns_friend_requests_to()
     {
         $sender = factory('App\User')->create();
@@ -173,7 +171,7 @@ class FriendshipTest extends TestCase
 
         $recipients[0]->acceptFriend($sender);
 
-        $this->assertCount(2 , $sender->friendRequestTo());
+        $this->assertCount(2, $sender->friendRequestTo());
         $this->containsOnlyInstancesOf(\App\User::class, $sender->friendRequestTo());
     }
 
@@ -188,7 +186,7 @@ class FriendshipTest extends TestCase
 
         $recipient->acceptFriend($senders[0]);
 
-        $this->assertCount(2 , $sender->friendRequestFrom());
+        $this->assertCount(2, $sender->friendRequestFrom());
         $this->containsOnlyInstancesOf(\App\User::class, $sender->friendRequestFrom());
-    }    
+    }
 }
