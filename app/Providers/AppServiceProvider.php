0890041c2b348e165089b05cb2360b316062dc22
Amr <merodiro@gmail.com>
2017-05-08 06:33:11 +0200

    use mysql for testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
         // \DB::listen(function ($query) {
         //     \Log::info($query->sql);
         // });
+
+        Schema::defaultStringLength(191);
 
     }
 
