24769aa3df0f0221f07a96ccf2694c7b6cc43bcc
Amr A.Mohammed <merodiro@gmail.com>
2018-01-23 00:11:08 +0200

    fix tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    @settings('name')
+                    @settings('name', 'Social network')
                 </div>
 
                 <div class="links">
