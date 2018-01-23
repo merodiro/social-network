b06fed5acbe12bf4a42b75515c01300110269a38
Amr A.Mohammed <merodiro@gmail.com>
2018-01-23 17:25:19 +0200

    fix code style

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Merodiro\Settings\Facades\Settings;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
